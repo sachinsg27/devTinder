e0685bd3e6ef94eca00d28cc6d67e1a40ebcdd1e
Sachin Ghutukade <sachinghutukade2704@gmail.com>
2025-04-06 13:45:51 +0530

    created expres server

---
+++ src/app.js
@@ -0,0 +1,35 @@
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Hello from the Dashboard");
+// });
+
+// app.get("/test", (req, res) => {
+//   res.send("Test Test Test....");
+// });
+
+// app.listen(7777, () => {
+//   console.log("Server listening on port 7777...");
+// });
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Dashboard");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Welcome to Testing");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello Hello Hello");
+});
+
+app.listen("7777", () => {
+  console.log("Server listening on port 7777...!");
+});
