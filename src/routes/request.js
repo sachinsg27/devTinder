0923f603c60846f1eac5de74b3efc160dbed18b4
Sachin Ghutukade <sachinsg2705@gmail.com>
2025-05-05 13:36:51 +0530

    creating req/review API

--- src/routes/request.js
+++ src/routes/request.js
@@ -52,4 +52,42 @@ requestRouter.post(
   }
 );
 
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      // check user is loggedIn or not
+      const loggedInUser = req.user;
+      // getting status and requestId from request
+      const { status, requestId } = req.params;
+      // check for status
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({ message: "Status not allowed!" });
+      }
+
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: "Connection request not found!" });
+      }
+
+      // modifying the status
+      connectionRequest.status = status;
+
+      // saving to DB
+      const data = await connectionRequest.save();
+
+      res.json({ message: `Connection request ${status}`, data });
+    } catch (err) {
+      res.status(400).send("ERROR : " + err.message);
+    }
+  }
+);
 module.exports = requestRouter;
