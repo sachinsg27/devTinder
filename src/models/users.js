40b091d161657860c0306507feae6c27213ebe4d
Sachin Ghutukade <sachinsg2705@gmail.com>
2025-05-05 11:19:44 +0530

    Querries and indexes

--- src/models/users.js
+++ src/models/users.js
@@ -40,10 +40,14 @@ const userSchema = mongoose.Schema(
     },
     gender: {
       type: String,
-      validate(value) {
-        if (!["male", "female", "others"].includes(value)) {
-          throw new Error("Gender data is not valid");
-        }
+      // validate(value) {
+      //   if (!["male", "female", "others"].includes(value)) {
+      //     throw new Error("Gender data is not valid");
+      //   }
+      // },
+      enum: {
+        values: ["male", "female", "other"],
+        message: `(VALUE) is not a valid gender type`,
       },
     },
     photoUrl: {
@@ -60,6 +64,8 @@ const userSchema = mongoose.Schema(
 
   { timestamps: true }
 );
+
+userSchema.index({ firstName: 1, lastName: 1 });
 
 userSchema.methods.getJWT = async function () {
   const user = this;
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,12 +1,55 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/users");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnection", userAuth, async (req, res) => {
-  const user = req.user;
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
 
-  res.send(user.firstName + "sent connection request");
-});
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status Type:" + status });
+      }
+
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        res.status(404).json({ message: "User Not Found" });
+      }
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionRequest) {
+        res.status(400).send({ message: "Connection request already exist!!" });
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      const data = await connectionRequest.save();
+      res.json({
+        message:
+          req.user.firstName + " is " + status + " in " + toUser.firstName,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("ERROR : " + err.message);
+    }
+  }
+);
 
 module.exports = requestRouter;
